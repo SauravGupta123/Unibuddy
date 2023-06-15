0d104d06ae668ae4589744de9dc2bb908d9f3610
Saurav <er.sauravgpt@gmail.com>
2023-06-16 00:34:56 +0530

    admin control component is only rendered for admins

--- src/components/NavSlider/NavSlider.jsx
+++ src/components/NavSlider/NavSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ggsipuLogo from '../../assets/ggsipu-logo.png';
 import { Link } from 'react-router-dom';
 
-export default function NavSlider() {
+export default function NavSlider({isAdmin}) {
   return (
     <div className="flex flex-col h-screen w-56 bg-[#29292a] text-white items-center">
 
@@ -18,15 +18,11 @@ export default function NavSlider() {
       <div>
         <ul className='flex flex-col space-y-4 mt-8 text-xl'>
           <li> <Link to='/' > Dashboard </Link> </li>
-          <li><Link to='/Notices' > Notices </Link></li>
-          <li>Examinations
-            <ul className="relative group text-lg">
-              <li> <Link to='/Results' > Results </Link></li>
-              <li>Admit Card</li>
-            </ul>
-          </li>
-          <li><Link to='/Resources' > Resources </Link></li>
-          <li><Link to='/TimeTable' > TimeTable </Link></li>
+          <li><Link to='/users/Notices' > Notices </Link></li>
+          <li> <Link to='/users/Results' > Results </Link></li>
+          <li><Link to='/users/Resources' > Resources </Link></li>
+          <li><Link to='/users/TimeTable' > TimeTable </Link></li>
+          {isAdmin &&  <li><Link to='/admin/controls' > Entire controls </Link></li> }
         </ul>
       </div>
     </div>
