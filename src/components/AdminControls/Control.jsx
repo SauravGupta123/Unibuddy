ea986806457962cfafe45ae1e7c4b5d5c58ca299
Saurav <er.sauravgpt@gmail.com>
2023-06-16 00:36:41 +0530

    Admin Controls Route is setup

---
+++ src/components/AdminControls/Control.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Control() {
+  return (
+    <div className='bg-blue-100   space-x-6 w-full p-8'>
+      <button  className='btn btn-blue '>Add  notices</button>
+      <button className='btn btn-blue'>Add Resources</button>
+      <button className='btn btn-blue'> Change timeTable</button>
+
+    </div>
+  )
+}
+
+export default Control;
