1ce83c9bbe2c76f2476dfc66b2f996199ee30f7e
Saurav <er.sauravgpt@gmail.com>
2023-06-16 14:53:06 +0530

    admin component is added

---
+++ src/components/AdminControls/Notices.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Notices() {
+  return (
+    <div className='bg-blue-400 mt-7  w-full h-[100vh]'>
+    this is Add noties
+  
+      
+    </div>
+  )
+}
+
+export default Notices
