510dfba774723a5e6f79507f977d9457314d55fb
Saurav <er.sauravgpt@gmail.com>
2023-06-16 14:53:30 +0530

    minor changes

--- src/components/AdminControls/Control.jsx
+++ src/components/AdminControls/Control.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Notices from './Notices';
 
 function Control() {
-  return (
-    <div className='bg-blue-100   space-x-6 w-full p-8'>
-      <button  className='btn btn-blue '>Add  notices</button>
-      <button className='btn btn-blue'>Add Resources</button>
-      <button className='btn btn-blue'> Change timeTable</button>
+  const [showNotices, setShowNotices] = useState(false);
 
+  const handleClick = () => {
+    if(showNotices){
+      
+    setShowNotices(false)}
+    else{
+      setShowNotices(true)
+    }
+
+  };
+
+  return (
+    <div className='bg-blue-100 space-x-6 w-full p-8'>
+      <button onClick={handleClick} className='btn btn-blue'>
+        Add notices
+      </button>
+
+      <button className='btn btn-blue'>Add Resources</button>
+      <button className='btn btn-blue'>Change timeTable</button>
+
+      {showNotices && <Notices />}
     </div>
-  )
+  );
 }
 
 export default Control;
