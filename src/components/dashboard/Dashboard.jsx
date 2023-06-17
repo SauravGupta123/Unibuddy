09fb56cf4c03198a034ddf7ee34815057452e374
Saurav <er.sauravgpt@gmail.com>
2023-06-18 00:26:24 +0530

    added changePassword button and its functionality

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Dashboard.css"
 import { Navigate, useNavigate } from "react-router-dom"
+import ChangePassword from "./ChangePassword";
+
+
 
 const Dashboard = (props) => {
+    const [showChangeComponent, setShowChangeComponent]=useState(false);
     const navigate = useNavigate()
     const handleLogout = () => {
         alert("logged Out Successfully")
         props.setLoginUser({ _id: null });
         console.log(props.user);
+    }
+    const handleClick=()=>{
+        if(showChangeComponent){
+
+            setShowChangeComponent(false)}
+            else{
+              setShowChangeComponent(true)
+            }
     }
     return (
         <div className="container bg-blue-200-300 w-full p-28">
@@ -18,10 +30,12 @@ const Dashboard = (props) => {
             <h2>
                 enrollment no= {props.user.enrollmentNo}
             </h2>
+            <button className="btn btn-blue mr-5 " onClick={handleClick} >Change password</button>
             <button className="btn btn-blue" onClick={handleLogout}> Log Out</button>
           
             </div>
 
+            {showChangeComponent && <ChangePassword enrollNo={props.user.enrollmentNo}/>}
         </div>
     )
 }
