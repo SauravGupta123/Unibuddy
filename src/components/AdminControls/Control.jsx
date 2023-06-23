2ebcf8b35ca437680ef2f72fdc6205aeb6599ed8
Saurav <er.sauravgpt@gmail.com>
2023-06-23 23:37:46 +0530

    on/off state for notices, resources components is handled by using values 1,2,3 in showComponent state for notices, resources and timetable respectively

--- src/components/AdminControls/Control.jsx
+++ src/components/AdminControls/Control.jsx
@@ -1,29 +1,55 @@
 import React, { useState } from 'react';
 import Notices from './Notices';
+import AddResources from './AddResources';
 
 function Control() {
-  const [showNotices, setShowNotices] = useState(false);
+  const [showComponent, setShowComponent] = useState(0);
 
-  const handleClick = () => {
-    if(showNotices){
+  const handleNoticeClick = () => {
+    if(showComponent!==1){
 
-    setShowNotices(false)}
+    setShowComponent(1)}
     else{
-      setShowNotices(true)
+      setShowComponent(0)
     }
 
   };
 
+  
+  const handleResourceClick = () => {
+    if(showComponent!==2){
+
+    setShowComponent(2)}
+    else{
+      setShowComponent(0)
+    }
+
+  };
+
+  const handleTimeTableClick = () => {
+    if(showComponent!==3){
+
+    setShowComponent(3)}
+    else{
+      setShowComponent(0)
+    }
+
+  };
+
+
+
   return (
     <div className='bg-blue-100 space-x-6 w-full p-8'>
-      <button onClick={handleClick} className='btn btn-blue'>
-        Add notices
-      </button>
 
-      <button className='btn btn-blue'>Add Resources</button>
-      <button className='btn btn-blue'>Change timeTable</button>
+      <button onClick={handleNoticeClick} className='btn btn-blue'>Add notices</button>
 
-      {showNotices && <Notices />}
+      <button onClick={handleResourceClick} className='btn btn-blue'>Add Resources</button>
+      <button  onClick={handleTimeTableClick} className='btn btn-blue'>Change timeTable</button>
+
+      {showComponent==1 && <Notices />}
+      {showComponent==2 && <AddResources />}
+      {/* {showComponent==2 && <EditTimeTable />} */} {/*-->todo*/}
+      {/* 1=notices,2=Resources,3=timeTable    */}
     </div>
   );
 }
